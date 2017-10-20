59c851bd74b82c197359220b98844b0af1b7ce28
Dani <dani.dalfo.f@gmail.com>
2017-10-20 18:05:19 +0200

    Classe singleton implementada

--- Singleton/src/Main/Singleton.java
+++ Singleton/src/Main/Singleton.java
@@ -1,13 +1,50 @@
 package Main;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.ResourceBundle;
+
 
 public class Singleton {
 
     private static Singleton instance = null;
-    private ResourceBundle cat;
     private ResourceBundle es;
+    private ResourceBundle cat;
     private ResourceBundle en;
     private HashMap<String,ResourceBundle> llistaresources;
+
+    protected Singleton()
+    {
+        llistaresources = new HashMap<String, ResourceBundle>();
+
+    }
+
+    private ResourceBundle resources(String lengua)
+    {
+        ResourceBundle tmp = llistaresources.get(lengua);
+        if(tmp==null)
+        {
+            tmp = ResourceBundle.getBundle(lengua);
+            llistaresources.put(lengua,tmp);
+        }
+        return tmp;
+    }
+
+    public static Singleton getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public String getText(String lengua, String key)
+    {
+        String ret=null;
+
+        ret = resources(lengua).getString(key);
+
+        return ret;
+    }
 }
