1e4f8c603a1af503573eba0599cbc6b379084a00
Dani <dani.dalfo.f@gmail.com>
2017-10-20 16:34:15 +0200

    Primer commit, resources i classe singleton

---
+++ Singleton/src/Main/Singleton.java
@@ -0,0 +1,13 @@
+package Main;
+
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+public class Singleton {
+
+    private static Singleton instance = null;
+    private ResourceBundle cat;
+    private ResourceBundle es;
+    private ResourceBundle en;
+    private HashMap<String,ResourceBundle> llistaresources;
+}
